7539d4a7408cf9a7737a8cd365210b7d2ff3e1a0
kelvinsekx <utdkelvin@gmail.com>
2023-04-04 02:56:47 -0700

    set header and paragraph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-3xl font-bold underline text-yellow-500'>Hello Ibadan Techies Club</h1>
+      <header>Hello Ibadan Techies Club</header>
     </>
   )
 }
