1ced29f78f2d58de49c1f5d84a83519154e1c4d0
kelvinsekx <utdkelvin@gmail.com>
2023-04-05 07:32:42 -0700

    add prettier rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>Hello Ibadan Techies Club</header>
+      <header>Hello "Ibadan" Techies Club</header>
     </>
   )
 }
