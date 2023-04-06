a60c001d9e45b400d26ee52945db042113ab1642
kelvinsekx <utdkelvin@gmail.com>
2023-04-06 01:26:39 -0700

    resolve conflicts

--- src/assets/svg.ts
+++ src/assets/svg.ts
@@ -1,2 +1,3 @@
-export { default as ArrowDecRight } from "./ArrowDecoRight.svg";
-export { default as ArrowDecLeft } from "./ArrowDecoLeft.svg";
+export { default as ArrowDecRight } from './ArrowDecoRight.svg'
+export { default as ArrowDecLeft } from './ArrowDecoLeft.svg'
+export { default as ArrowRight } from './arrow-right.svg'
