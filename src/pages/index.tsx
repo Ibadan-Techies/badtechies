50fddd7e6c42d1d2cf589303c389d332fb790bfd
kelvinsekx <utdkelvin@gmail.com>
2023-04-08 23:01:09 -0700

    EventCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Footer from '@/components/Footer/footer'
 import NavigationBar from '@/components/Navbar/navbar'
-// import LandingBanner from '@/components/LandingBanner/banner'
-// import AboutUs from '@/components/LandingAboutUs/about'
+import LandingBanner from '@/components/LandingBanner/banner'
+import AboutUs from '@/components/LandingAboutUs/about'
 
 export default function Home() {
   return (
@@ -14,8 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavigationBar />
-      {/* <LandingBanner />
-      <AboutUs /> */}
+      <LandingBanner />
+      <AboutUs />
       <Footer />
     </>
   )
