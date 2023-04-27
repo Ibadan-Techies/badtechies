c3627f83f1d883feee3737965cb7639f971ed36f
kelvinsekx <utdkelvin@gmail.com>
2023-04-27 01:41:25 -0700

    rewrite OvelImageFrame to use next/image

--- src/components/LandingAboutUs/frame.tsx
+++ src/components/LandingAboutUs/frame.tsx
@@ -1,25 +1,26 @@
-import Image from 'next/image'
 import OvalImageFrame from '../OvalFrame/oval-frame'
 
 const Frame = () => {
   return (
     <div className="flex items-baseline xl:w-[40rem] lg:w-[20rem] lg:h-[16rem] lg:m-0 my-10 lg:absolute lg:bottom-0 xl:right-12 lg:right-1 w-fit gap-3 self-center">
-      <div className="relative -bottom-10">
+      <div className="relative top-16">
         <OvalImageFrame
           bgColor="green"
           src="https://res.cloudinary.com/dfliypjzt/image/upload/q_auto,f_auto,fl_lossy/v1681297044/gallery/ogb1_kxukql"
         />
       </div>
-      <div className="relative bottom-10">
+      <div className="relative -top-8">
         <OvalImageFrame
           bgColor="red"
           src="https://res.cloudinary.com/dfliypjzt/image/upload/q_auto,f_auto,fl_lossy/v1681297044/gallery/thegangshit_bqixgy"
         />
       </div>
-      <OvalImageFrame
-        bgColor="yellow"
-        src="https://res.cloudinary.com/dfliypjzt/image/upload/q_auto,f_auto,fl_lossy/v1681297043/gallery/meku_d8ruqd"
-      />
+      <div>
+        <OvalImageFrame
+          bgColor="yellow"
+          src="https://res.cloudinary.com/dfliypjzt/image/upload/q_auto,f_auto,fl_lossy/v1681297043/gallery/meku_d8ruqd"
+        />
+      </div>
     </div>
   )
 }
