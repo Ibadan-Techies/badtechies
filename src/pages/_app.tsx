64d4cd5473300a5b9a926fcba6c1ff068d30f34c
kelvinsekx <utdkelvin@gmail.com>
2023-05-04 08:50:49 -0700

    reverts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import SEO from '../../next-seo.config'
+import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={` font-sans`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+      <div className={` font-sans`}>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,8 +18,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body className="* box-border m-0 p-0 overflow-wrap break-word focus:shadow-outline-gray">
+      <body className="* box-border m-0 p-0 overflow-wrap break-word focus:shadow-outline-gray antialiased">
         <Main />
         <NextScript />
       </body>
