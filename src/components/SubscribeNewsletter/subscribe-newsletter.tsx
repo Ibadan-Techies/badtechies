ee6b6846402456ed00cab7f524d722f96a4a9029
kelvinsekx <utdkelvin@gmail.com>
2023-05-04 08:39:10 -0700

    revert changes

--- src/components/SubscribeNewsletter/subscribe-newsletter.tsx
+++ src/components/SubscribeNewsletter/subscribe-newsletter.tsx
@@ -1,8 +1,54 @@
+import * as React from 'react'
 import { SquiggleLeft, SquiggleRight } from '@/assets/svg'
 import Button from '../Button/button'
 import { Container } from '../Layout/Container'
 
+const headers = {
+  Authorization: 'Token df8cf218-3976-4e75-9ee6-017a6ffe2d4f',
+}
+const BASE_URL = 'https://api.buttondown.email'
+const ENDPOINT = '/v1/subscribers'
+
+const { useState } = React
 function SubscribeNewsLetter() {
+  const [formState, setFormState] = useState('submit')
+  const [email, setEmail] = useState('')
+  const [dis, setDis] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target
+    setEmail(target.value)
+  }
+
+  // function encode(data: string[]) {
+  //   return Object.keys(data)
+  //     .map(
+  //       (key: number) =>
+  //         encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+  //     )
+  //     .join('&')
+  // }
+
+  const handleSubmit = (e: Event) => {
+    setFormState('processing...')
+    setDis(true)
+    const form = e.target
+    fetch(BASE_URL + ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Authorization: 'Token df8cf218-3976-4e75-9ee6-017a6ffe2d4f',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: `${email}` }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error()
+        alert(
+          'Wait a sec, your data is collected. Sharpview will reach back to you soon. Bye!!!'
+        )
+      })
+      .catch((err) => alert(err))
+  }
   return (
     <Container bg="bg-ibtc-light-yellow">
       <div className="flex flex-col items-center justify-center min-h-[25rem] gap-[4rem]">
@@ -16,13 +62,16 @@ function SubscribeNewsLetter() {
         <div className="w-full items-center flex flex-col lg:flex-row justify-center lg:px-4 gap-[1.15rem] sm:max-w-[400px] lg:max-w-full">
           <input
             type="email"
-            name="your-email"
+            onChange={handleChange}
+            value={email}
+            name="email"
             placeholder="Enter your email"
             aria-label="Enter your email"
             className="bg-transparent w-full border border-[#BDBDBD] rounded-[6px] h-[3rem] lg:h-[3.5rem] lg:max-w-[28rem] px-2 placeholder:text-[#828282] outline-4 outline-transparent outline-offset-[6px] focus:outline-ibtc-blue"
           />
           <span className="w-full lg:w-fit h-[3rem] lg:h-[3.5rem]">
             <Button
+              onClick={handleSubmit}
               type="button"
               style={{
                 color: 'text-ibtc-grey',
