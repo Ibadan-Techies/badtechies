5d82a7ae4cc63c9e0de5b3e2a09dc918ae0bfc59
kelvinsekx <utdkelvin@gmail.com>
2023-05-06 22:54:49 -0700

    resduce eventcard a bit

--- src/components/EventCard/eventcard.tsx
+++ src/components/EventCard/eventcard.tsx
@@ -7,7 +7,7 @@ const EventCard = ({
   details: { imgSrc: string; topic: string; date: string; speaker: string }
 }) => {
   return (
-    <div className="relative lg:w-[19.6rem] w-full lg:h-[25.1875rem] h-[23rem]">
+    <div className="relative lg:w-[19.6rem] w-full lg:h-[24.1875rem] h-[23rem]">
       <Image
         src={imgSrc}
         alt=""
